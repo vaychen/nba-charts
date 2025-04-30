462d748d80c331f3669067a4995008917dfdcabb
Puck Chen <Puck_Chen@bose.com>
2025-04-30 14:21:00 +0800

    Use dml and ddl sql scripts to store query, instead of writing in python

--- database/ddl/database.sql
+++ database/ddl/database.sql
@@ -3,13 +3,3 @@ create database nba_charts;
 
 -- create schema stats
 create schema stats;
-
--- create table players
-create table stats.players (
-    player_index_id int primary key,
-    player_index_last_name varchar(256) not null,
-    player_index_first_name varchar(256),
-    player_index_full_name varchar(256),
-    player_index_is_active boolean,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
